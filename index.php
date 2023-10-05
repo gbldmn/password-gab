99491ed1f1c11e68ca7003e2267ef4762ee766f1
glbdmn <gabrieledamiani@hotmail.it>
2023-10-05 15:49:32 +0200

    creazione repo e impostazioni iniziali

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="file.css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="box d-flex flex-column ">
+        <div class="item">
+
+        </div>
+        <div class="item1">
+
+        </div>
+    </div>
+    
+</body>
+</html>
